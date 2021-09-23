77c89767353c576e931638f3677390bacb9819e7
michaelliu118 <49260981+michaelliu118@users.noreply.github.com>
2021-09-22 23:16:21 -0400

    change dropdown bar

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -12,33 +12,33 @@ function App() {
   const selections = ['DIR', 'IR'];
   const [initial_page, updateNav] = useState(selections[0])
   const [json_object, update_json_object] = useState(0);
+  const ac_models = ['CRJ700', 'CRJ900', 'CRJ1000']
 
   function fetching_heatmap(){
     fetch('http://localhost:5000/', {
       method:'GET',
       credentials:'include',
       mode: 'cors',
-    }).
-    then(
+    }).then(
       res=>res.json()
-      ).
-    then(
+      ).then(
       data=>{
         update_json_object(data);
-    }).
-    catch(error => {
+    }).catch(error => {
       console.log('Download Error: ', error);
     })
   };
 
   useEffect(fetching_heatmap, []);
-
+  console.log('appp')
 
   //Display different heatmaps
   function Display() {
     if (initial_page === 'DIR'){
       return (
-      <HeatmapWindow name={'DIR'} table_html={json_object===0 ? "" : json_object['DIR']} colorBar_html={json_object===0 ? "<div />" : json_object['DIR_colorBar']}/>
+      <HeatmapWindow name={'DIR'} table_html={json_object===0 ? "" : json_object['DIR']} 
+      colorBar_html={json_object===0 ? "<div />" : json_object['DIR_colorBar']} 
+      ac_model={ac_models}/>
       );
     } else if (initial_page==='IR'){
       return (
--- src/content/content1/content1.jsx
+++ src/content/content1/content1.jsx
@@ -1,10 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './content1.css';
 
 
 function HeatmapWindow(props) {
-  
+
     console.log('updated updated')
+
+    //create the useState hook
+    const [table_html, update_table_html] = useState(props.table_html);
+    const [colorBar_html, update_colorBar_html] = useState(props.colorBar_html);
+    const [generate_button_state, update_generate_button_state] = useState('Generate Heatmap');
 
     //Create an array of past five years that is to be used at year selection dropdown bar
     const current_year = new Date().getFullYear();
@@ -12,22 +17,65 @@ function HeatmapWindow(props) {
     for (let i=5; i>0; i--){
       year_dropdown_bar.push(current_year-i);
     }
-    const showing_value_dropdown_bar = year_dropdown_bar[year_dropdown_bar.length-1];
-    year_dropdown_bar.splice(year_dropdown_bar.length-1, 1);
 
     //Create the array for 12 month in a year
     var month_dropdown_bar = [];
     for (let i=1; i<=12; i++){
-      month_dropdown_bar.push(i)
+      month_dropdown_bar.push(i);
     }
 
 
-    //create the useState hook
-    const [table_html, update_table_html] = useState(props.table_html);
-    const [colorBar_html, update_colorBar_html] = useState(props.colorBar_html);
-    const [generate_button_state, update_generate_button_state] = useState('Generate Heatmap');
+    //Initial dropdown bar showing values
+    var showing_month_dropdown_bar = month_dropdown_bar[0];
+    var showing_ac_model_dropdown_bar = props.ac_model[0];
+    var showing_year_dropdown_bar = year_dropdown_bar[year_dropdown_bar.length-1]
 
-     
+    async function update_dropdown_current_value() {
+      var year = null;
+      var month = null;
+      var model = null;
+      while (year===null || month===null || model===null){
+        await new Promise((resolve, reject)=>{setTimeout(resolve, 500)});
+        model = document.getElementById('ac_model_selection');
+        year = document.getElementById('year_selection');
+        month = document.getElementById('month_selection');
+      }
+      showing_ac_model_dropdown_bar = model.value;
+      showing_year_dropdown_bar = year.value;
+      showing_month_dropdown_bar = month.value;
+      model.className = 'ac_model_dropdown_bar';
+      year.className = 'year_dropdown_bar';
+      month.className = 'year_dropdown_bar'
+      console.log('wehhh'+showing_ac_model_dropdown_bar);
+    }
+    //update_dropdown_current_value()
+    useEffect(update_dropdown_current_value, [table_html])
+
+  
+    function dropdown_onChange(id){
+      const element = document.getElementById(id);
+      const current_value = element.value;
+      if (id==='ac_model_selection'){
+        if (current_value!==showing_ac_model_dropdown_bar){
+          element.className = 'ac_model_dropdown_bar_on_change';
+        } else {
+          element.className = 'ac_model_dropdown_bar';
+        }
+      } else if (id==='year_selection'){
+        if (current_value!==showing_year_dropdown_bar){
+          element.className = 'year_dropdown_bar_on_change';
+        } else {
+          element.className = 'year_dropdown_bar';
+        }
+      } else {
+        if (current_value!==showing_month_dropdown_bar){
+          element.className = 'year_dropdown_bar_on_change';
+        } else {
+          element.className = 'year_dropdown_bar';
+        }
+      }
+    }
+
     //This function is used to fetch heatmap when user click on generate button
     async function fetching_heatmap(){
       //Change the button state to loading
@@ -38,10 +86,12 @@ function HeatmapWindow(props) {
       //get user selected year and month
       const current_selected_year = document.getElementById('year_selection').value;
       const current_selected_month = document.getElementById('month_selection').value;
+      const current_selected_ac_model = document.getElementById('ac_model_selection').value;
       const data = new FormData();
       data.append('year', current_selected_year);
       data.append('month', current_selected_month);
       data.append('metric', props.name);
+      data.append('ac_model', current_selected_ac_model);
 
       //fetch the heatmp of past 12 month from select month
       const post = await fetch('http://localhost:5000/', {
@@ -70,11 +120,16 @@ function HeatmapWindow(props) {
         <span className='heatmap-title-span'>{props.name}</span>
         <div className='heatmap-year-legend-container'>
             <div className='date-selection-container'>
+              <span style={{fontSize: "18px", marginBottom:"10px"}}>Select AC Model:</span>
+              <select className='ac_model_dropdown_bar' id='ac_model_selection' onChange={()=>{dropdown_onChange('ac_model_selection')}}>
+                {props.ac_model.map((value, index)=>{
+                  return (<option key={index}>{value}</option>);
+                })}
+              </select>
+            </div>
+            <div className='date-selection-container'>
               <span style={{fontSize: "18px", marginBottom:"10px"}}>Select Year:</span>
-              <select className='year_dropdown_bar' id='year_selection'>
-                <option value={showing_value_dropdown_bar}>
-                  {showing_value_dropdown_bar}
-                </option>
+              <select className='year_dropdown_bar' id='year_selection' defaultValue={year_dropdown_bar[year_dropdown_bar.length-1]} onChange={()=>{dropdown_onChange('year_selection')}}>
                 {year_dropdown_bar.map((value, index)=>{
                   return (<option key={index}>{value}</option>);
                 })}
@@ -82,7 +137,7 @@ function HeatmapWindow(props) {
             </div>
             <div className='date-selection-container'>
               <span style={{fontSize: "18px", marginBottom:"10px"}}>Select Month:</span>
-              <select className='year_dropdown_bar' id='month_selection'>
+              <select className='year_dropdown_bar' id='month_selection' onChange={()=>{dropdown_onChange('month_selection')}}>
                 {month_dropdown_bar.map((value, index)=>{
                   return (<option key={index}>{value}</option>);
                 })}
